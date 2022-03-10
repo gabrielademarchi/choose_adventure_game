9ccb6f80866384585bb6190da0ba3bdb332b7895
Gabriela Demarchi <64430799+ghdemarchi@users.noreply.github.com>
2022-03-09 23:14:10 -0300

    Adicionando git ignore e exercício

---
+++ main_alice.py
@@ -0,0 +1,22 @@
+print('Most adventures are about other people.\n\
+This one is about you -- and a girl named Alice in a place called Wonderland!')
+
+input('Are you ready? (press any key)')
+
+print('''
+           .'\   /`.
+         .'.-.`-'.-.`.
+    ..._:   .-. .-.   :_...
+  .'    '-.(o ) (o ).-'    `.
+ :  _    _ _`~(_)~`_ _    _  :
+:  /:   ' .-=_   _=-. `   ;\  :
+:   :|-.._  '     `  _..-|:   :
+ :   `:| |`:-:-.-:-:'| |:'   :
+  `.   `.| | | | | | |.'   .'
+    `.   `-:_| | |_:-'   .'
+      `-._   ````    _.-'
+          ``-------''
+''')
+
+
+print('''It is a late summer day, and you are sitting by the bank of a pe''')
