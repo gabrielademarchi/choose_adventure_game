b7bd58a4abc8f56e5b7cdb656e164765a8978aa2
Gabriela Demarchi <64430799+ghdemarchi@users.noreply.github.com>
2022-03-10 10:17:51 -0300

    Adding main_alice

--- main_alice.py
+++ main_alice.py
@@ -1,14 +1,11 @@
-print('Most adventures are about other people.\n\
-This one is about you -- and a girl named Alice in a place called Wonderland!')
-
-input('Are you ready? (press any key)')
+print('Welcome to Choose your Adventure - Alice in Wonderland.')
 
 print('''
            .'\   /`.
          .'.-.`-'.-.`.
     ..._:   .-. .-.   :_...
-  .'    '-.(o ) (o ).-'    `.
- :  _    _ _`~(_)~`_ _    _  :
+  .'    '-.(o  (o .-'    `.
+ :  _    _ _`~(_~`_ _    _  :
 :  /:   ' .-=_   _=-. `   ;\  :
 :   :|-.._  '     `  _..-|:   :
  :   `:| |`:-:-.-:-:'| |:'   :
@@ -16,7 +13,284 @@ print('''
     `.   `-:_| | |_:-'   .'
       `-._   ````    _.-'
           ``-------''
-''')
+    ''')
+
+page_0 = '''It is a lazy summer day, and you are sitting by the bank of a peaceful river, enjoying the sunshine. 
+You sigh and lean back against a tree. You are feeling happy and drowsy. You close your eyes...'''
+
+page_1 = '''You are just dozing off when you hear someone shout, "Wait!"
+You open your eyes and see a girl running by. She looks as if she's chasing someone.
+You jump up and run alongside her. The girl looks at you in surprise. 
+"I haven't got time to talk", she says. "I might lose him!"'''
+
+page_2 = '''Lose who? you ask. "Who are you chasing?"
+"The White Rabbit!" answers the girl.
+You are about to ask her why she is chasing a white rabbit when she cries, "Look! He's going in that hole!"
+And without another word, the girl crawls into a hole nearby. You don't know whats going on, but you want to find out.
+"Wait for me!" you shout.'''
+
+page_3 = '''You crawl into the hole, too - and find yourself falling through space! But you are falling in slow motion.
+Presently you notice that you are floating past cupboards and shelves, clocks and mirrors. Maybe you could grab for something and break your fall.
+Or could let yourself continue to float downward.'''
+
+page_4 = '''You grab for a cubboard and scramble inside. But you are surprised to find that the inside of the cupboard is a long tunnel spiraling downward.
+You try to crawl back out, but the sides are too steep. You slide down at breakneck speed!
+Suddenly you shoot out the end - and land right on top of someone!'''
+
+page_6 = '''You float down and down until you land with a thump at the bottom of the hole. You look around for the girl, but she's nowhere in sight. In front of you is a passage with doors all along it.
+You run to the end and reach another passagewaay. This one is flooded.
+"Now, where did she go?" you say aloud.
+"Through my keyhole, into my garden!" saus a doorknob.'''
+
+page_7 = '''You don't know who is underneath you, but you look up and recognize the girl you followed down the rabbit hole. 
+And suddenly you realize that you and the girl are surrounded by giant playing cards!
+You must have gotten smaller! What's going on? Is it magic, you wonder?
+The girl looks at you. "Oh, dear!" she says. "I'm afraid you just fell on the Queen of Hearts! Let's run before she gets mad and has our heads!"'''
+
+page_8 = '''You dash after the girl. "What's your name?" you ask.
+"Alice," she replies.
+"I'm very glad to meet you," you say.
+"I'm glad to meet you, too!"
+You and Alice run through a dark forest until you come to a fork in the road.'''
+
+page_9 = '''"Which way should we go?" says Alice.
+suddenly you notice two signs on a tree. One says THIS WAY, and the other says THAT WAY.
+"Well?" asks Alice.
+You shrug and say, "We can either go THIS WAY or THAT WAY, I think we should go...'''
+
+page_10 = '''You jump with surprise. Atalking doorknob!
+But you recover quickly and say, "How can I get into your garden?"
+"Just wish you were ther!" says the doorknob. "I find it's much simpler that way."
+You can't believe it will be that easy, but you close your eyes and wish, anyway.
+When you open them again, you find yourself in a beautiful garden.
+"It worked!" you cry.'''
+
+page_11 = '''Just then you see the girl again. You have both shrunk to the size of the flowers in the garden.
+"Hello," you say. "What's your name?"
+"Alice," answers the girl.
+At that very moment the White Rabbit runs by - and he's all dressed up! You ask Alice what she wants to do.
+"What do you think we should do?" she ask'''
+
+page_12 = '''You and Alice follow the path to THAT WAY. You have gone only a little way when you see a Cheshire Cat on the brancj of a tree.
+"Excuse me, Cheshire Puss," you say, "are we going the right way for THAT WAY?"
+Instead of answering, the Cheshire Cat just grins.'''
+
+page_13 = '''"Excuse me," you say again, "but we would like to kno where THAT WAY leads to."
+The cat just keeps grinning from ear to ear.
+"Excuse me, too," says Alice timidly, "but why are you grinning?"
+"Why are you asking so many questions?" says the cat, pointing to Alice.
+"Because we want to know where THAT WAY leads to!" you say a bit angrily.'''
+
+page_14 = '''You and Alice chase after the White Rabbit. He runs down a path and into a small house, all the while saying, "Oh, my fur and whiskers, I'm late! I'm late!"
+You are just about to follow the White Rabbit into the house when he dashes out again.
+"Don't just stand there," he calls to Alice. "Go inside and fetch my gloves!"'''
+
+page_15 = '''Alice is so surprised that she runs into the house to do as the White Rabbit asks.
+"Wais for me!" you yell.
+You and Alice look everywhere, byt you can't find the White Rabbit's gloves.
+"I don't think we're going to find them at all," says Alice.
+You can't find the gloves, but you do find a cookie jar labeled TAKE ONE.'''
+
+page_16 = '''The cover is on tightly, however, and you will have to work to get it off.
+From outside you hear the White Rabbit call, "Hurry, please. Seconds count. The Queen will be very angry if I'm late!"'''
+
+page_17 = '''You don't want the White Rabbit to get into trouble, but as Alice said, you probably won't ever find his gloves. And you are hungry. 
+Should you stop to eat some cookies, or should you keep looking for the gloves?'''
+
+page_18 = '''The White Rabbit scampers away, and you and Alice turn and run in the opposite direction. 
+Soon you come to another garden.
+"What kind of flowers are you?" ask a flower.
+"We're not flowers," says Alice as a bread-and-butterfly glides by. She doesn't seem at all surprised to hear the flower speak.
+"Then you must be weeds!" says a daisy. "Be off with you!"'''
+
+page_19 = '''Talking flowers! Bread-and-butterflies! You've never seen anything like this in your life!
+But the flowers aren't very friendly, so you and Alice leave the garden and run down a tangled path.'''
 
 
-print('''It is a late summer day, and you are sitting by the bank of a pe''')
+page_20 = '''The path leads to a small beach, where a strange race is going on. It has no beginning and no end - everyone just runs around and around a rock.
+"Would you hold this?" asks Alice, as she hands you a little golden key. "I want to join the race."
+You hold the key in your hand and watch the runners. But the race is making you dizzy. You close your eyes for a moment...'''
+
+page_21 = '''...and when you open them, you're back on the riverbank. Was it all a dream?
+You feel something in your hand. You are still holding the key that Alice gave you!
+You wonder if you can find your way back to the long hall at the bottom of the rabbit hole. 
+You're sure this is the key that opens the door to that wonderful place!
+On the other hand, maybe if you just wish you were back...
+THE END'''
+
+page_22 = '''After some twisting and turning, you get the cover off the cookie jar. You are so jungry that you gobble up two big cookies before you offer one to Alice.
+Alice is nibbling on hers when you notive that the house has gotten smaller. No! You are getting bigger! It must be the cookies!'''
+
+page_23 = '''"Alice, help!" you yell, as your head pushes the roof off the house. You`re so big that there's no more room for alice in the house.
+She runs outside and looks at you helplessly as you grow up and up.
+Oh, why did you eat those cookies? Why didn't you just TAKE ONE, as the label said?
+Will you ever shrink to your proper size?
+You hope you won't be quite big when you wake up from your dream!
+THE END'''
+
+page_24 = '''You and Alice follow the overgrown path to THIS WAY. After walking a bit, you hear, "Stop!"
+You look up in surprise and see a funny-looking caterpillar sitting on a mushroom.
+"Why must we stop?" you ask politely. "We want to go THIS WAY."
+"Well, if you want to go THIS WAY," says the Caterpillar, "you will have to go THAT WAY to get there."'''
+
+
+page_25 = '''"But we don't want to go THAT WAY. That's why we picked THIS WAY!" says Alice, a little crossly.
+"I just told you," says the Caterpillar, a little crossly, too. 
+"You can't go THIS WAY this way."
+You and Alice look at each other. You want to go THIS WAY, but are afraid of making the Caterpillar mad - especially since he's so big!'''
+
+
+page_26 = '''"But we don't wnat to go THAT WAY," cries Alice. "And if we can't go THIS WAY, we can't go anywhere!"
+"Well, that's not true at all," says the Caterpillar. "There is always a where to go to when you can't go where you want to."
+"Really" you say. "Then tell us where."
+"Why you can go nowhere!" says the Caterpillar.
+You and Alice sigh and give up.
+"There! That's settled," says the Caterpillar.'''
+
+
+page_27 = '''"But don't worry," he continues. "I will entertain you. 
+I know all my multiplication tables and I'll recite them for you, one after the other."
+You groan and sit down. Why did you choose to go THIS WAY? Now you-re stuck with a boring caterpillar who lover arithmetic.
+What a way to spend the day... What a ways to end a dream!
+THE END'''
+
+page_28 = '''You look high and low fot the White Rabbit`s gloves. In one room you find a closet with a heavy door. You go inside - and the door slams shut behund you.
+You try to open it but you can't. You're locked in!
+"Help!" you cry, but you know that Alice can't hear you through the tick door. You will have to open it yourself.'''
+
+page_29 = '''You push and pull and tug, but the door won't budge. Then, in the light that shines in under the door, you see a can of oil labeled USE ME.
+You squirt the oil around the door and it swings open.
+You rush out of the closet and out of the house, calling for Alice, but she is nowhere to be found.'''
+
+page_30 = '''"Well, if you must know," says the Cheshire Cat, "THAT WAY leads to THE END."
+"That souns crazy," you say.
+"Of course it does," answers the cat. "That's because it is crazy. Everything here is crazy."
+"I think we should go somewhere else," says Alice.
+You would like to talk to the Cheshire Cat some more, but alice looks frightened.'''
+
+page_33 = '''You and Alice continnue along the path.
+Soon you come upon a big sign that says KEEP OUT BY ORDER OF TWEEDLEDUM. Beyond it lies a garden.
+"Who's Tweedledum?" asks Alice.
+"It's such a silly name," you answer. "I'm sure it's no one important."
+You want to folloe a path that goes through the garden, but Alice isn't sure that's a good idea. The only other path, though, peads through some thick bushes.'''
+
+page_34 = '''"What do you mean THAT WAY leads to THE END?" you ask the Cheshire Cat.
+But instead of answering, the cat starts to disappear@
+Soon all that is left is his grin.
+"Well," says Alice, "I've often seen a cat without a grin, but a grin withou a cat! 
+It's the most curious thing I ever saw all my life!"'''
+
+page_35 = '''"Me, too," you answer. "And by the way, I'm more curious than ever to find out where THAT WAY leads!"
+So you and Alice continue down the path.
+In a little while you come to a grove of Weeping Willoe trees.
+Suddenly Alice stubs her tow on a rock and starts to cry.'''
+
+page_36 = '''You are trying to make her fell better, when a big drop of water falls on your head. Then another... and another and another.
+It it raining? No! It's the Weeping Willows. They are crying with Alice! They cry so hard they start a samll flood!
+"Stop crying, Alice," you say. "You're making the Weeping Willows cry, too!"
+"I can't," sobs Alice. "It hurts too much!"'''
+
+page_37 = '''You try to help her up, but now the food of tears has turned into a small river. You and Alice are swept along like two leaves.
+Suddenly you hear a roar. Oh, no! There's a big waterfall ahead and you are heading right for it!
+"I hope this is all a dream," you say. "Otherwise going THAT WAY really is going to lead to...
+THE END'''
+
+page_38 = '''You step boldly into the garden, but Alice stands back. As soon as you enter, you feel a tug on your arm.
+"I'm Tweedledum," says a roly-poly little man as he pulls you out of the garden. "Keep out!"
+The you feel a tug on your other arm.
+"I'm Tweedledee, his brother," says a look-alike man, as he pulls you into the garden. "Keep in!"'''
+
+page_39 = '''You are wondering what's going on when you see the other side of the sign, the side that's facing the garden. It says KEEP IN BY ORDER OF TWEEDLEDEE.
+Just then, Tweedledum and Tweedledee pull at the same time. You feel as if you're stretching.
+You look at your arms - your are!
+"Help!" you cry to Alice, but there's nothing she can do.
+Oh, no! Now you're caught in a tug of war between two quarrelsome brothers, and you're the rope!
+THE END'''
+
+page_40 = '''When you come out on the other side of the thick bushes, you find yourselves in the middle of a tea party!
+A funny little man in a big hat shouts, "This is the March Hare's un-birthday party. Did you bring a present?"
+"Why, no," you both say.
+"Good!" says the little man. "Then you're un-invited!"'''
+
+page_41 = '''"The Mad Hatter is right," says the March Hare."You're un-invited, so sit down!"
+You and Alice sit down. In a few minutes you are caught up in the craziest party you've ever been to.
+Everyone keeps switching places and switching cups, and the Hatter and Hare stuff a dormouse into the teapot!
+You've neve had more fun at an un-birthday party in your life!
+THE END'''
+
+page_42 = '''After a while you come to a clearing - and there's Alice! But she seems to be in trouble. 
+She is surrounded by giant playing cards, and the Queen of Hearts is shouting, "Off with her head!"
+You rush up to Alice and say, "Can I help?"
+"Silence!" says the Queen, "or I'll have your head, too!"'''
+
+page_43 = '''"Don't worry about me," says Alice, handling you a mushroom. "Here, eat this quickly!"
+You take several biter and in a flash you grow to your full size. Then you see that the Queen and her soldiers are just an ordinary pack of playing cards.
+They're not giant at all.
+You start to swat them away from your feet, and suddenly you find yourself back on the riverbank swatting at a dragonfly!
+It was all a dream!
+THE END'''
+
+print(page_0, end='\n \n')
+print(page_1, end='\n \n')
+print(page_2, end='\n \n')
+print(page_3, end='\n \n')
+page_3_question = input(
+    'Do you grab for something (press G) or do you let yourself continue to fall (press F?) ')
+if (page_3_question == 'G') or (page_3_question == 'g'):
+    print('\n', page_4, end='\n \n')
+    print(page_7, end='\n \n')
+    print(page_8, end='\n \n')
+    print(page_9, end='\n \n')
+    page_9_question = input(
+        'Do you go THIS WAY (press 1) or THAT WAY (press 2?) ')
+    if page_9_question == '1':
+        print('\n', page_24, end='\n \n')
+        print(page_25, end='\n \n')
+        print(page_26, end='\n \n')
+        print(page_27, end='\n \n')
+    else:
+        print('\n', page_12, end='\n \n')
+        print(page_13, end='\n \n')
+        print(page_30, end='\n \n')
+        page_30_question = input(
+            'Do you go somewhere (S) or stay and talk to the cat (T)? ')
+        if (page_30_question == 'S') or (page_30_question == 's'):
+            print('\n', page_33, end='\n \n')
+            page_33_question = input(
+                'Do you go into the garden (G) or through the bushes (B?) ')
+            if (page_33_question == 'G') or (page_33_question == 'g'):
+                print('\n', page_38, end='\n \n')
+                print(page_39, end='\n \n')
+            else:
+                print('\n', page_40, end='\n \n')
+        else:
+            print('\n', page_34, end='\n \n')
+            print(page_35, end='\n \n')
+            print(page_36, end='\n \n')
+            print(page_37, end='\n \n')
+else:
+    print('\n', page_6, end='\n \n')
+    print(page_10, end='\n \n')
+    print(page_11, end='\n \n')
+    page_11_question = input(
+        'Do you decide to follow the Rabbit (R) or that you and Alice should go exploring (E)? ')
+    if (page_11_question == 'R') or (page_11_question == 'r'):
+        print('\n', page_14, end='\n \n')
+        print(page_15, end='\n \n')
+        print(page_16, end='\n \n')
+        print(page_17, end='\n \n')
+        page_17_question = input(
+            'Do you eat some cookies (C) or keep looking for the gloves (G)? ')
+        if (page_17_question == 'C') or (page_17_question == 'c'):
+            print('\n', page_22, end='\n \n')
+            print(page_23, end='\n \n')
+        else:
+            print('\n', page_28, end='\n \n')
+            print(page_29, end='\n \n')
+            print(page_42, end='\n \n')
+            print(page_43, end='\n \n')
+    else:
+        print('\n', page_18, end='\n \n')
+        print(page_19, end='\n \n')
+        print(page_20, end='\n \n')
+        print(page_21, end='\n \n')
